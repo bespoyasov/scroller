df4bb699c53b0c290e6f9258c2216576067ca2cf
Alexey Berezin <beraliv.spb@gmail.com>
2021-08-13 14:43:08 +0200

    refactor: ❌ remove private methods

--- build/scroller.d.ts
+++ build/scroller.d.ts
@@ -44,52 +44,8 @@ export interface ScrollerConstructor {
 }
 
 export interface ScrollerInstance {
-  // public
   scrollTo(point: ScrollToPoint, time?: number): void;
   update(config: ScrollerUpdateConfig): void;
-
-  // TODO: probably remove
-  // private
-  checkBorderVisibility(): void;
-  animate(
-    start: number,
-    stop?: number,
-    speed?: number,
-    animateWidth?: number
-  ): void;
-  handleTouchMove(event: TouchEvent): void;
-  handleTouchStart(event: TouchEvent): void;
-  onScrollbarPointerUp(event: MouseEvent | TouchEvent): void;
-  onScrollbarPointerMove(event: MouseEvent | TouchEvent): void;
-  onScrollbarPointerDown(event: MouseEvent | TouchEvent): void;
-  onAnchorClick(event: MouseEvent): void;
-  onScrollClick(event: MouseEvent): void;
-  onScroll(event: WheelEvent): void;
-  onKeyDown(event: KeyboardEvent): void;
-  onFocus(event: FocusEvent): void;
-  onClickLink(event: MouseEvent): void;
-  onPointerUp(event: MouseEvent): void;
-  onPointerMove(event: MouseEvent): void;
-  onPointerDown(event: MouseEvent | TouchEvent): void;
-  _update(): void;
-  checkScrollable(): void;
-  setSize(): void;
-  createAnchors(): void;
-  removeAnchors(): void;
-  findCentralNode(): void;
-  wrapItems(): void;
-  createWrapper(): void;
-  bindAnchorsEvents(): void;
-  init(element: HTMLElement): void;
-  clearPointerState(): void;
-  setWidth(width: number): void;
-  setPosition(element: HTMLElement | null, position: number): void;
-  setScbPos(position: number): void;
-  setPos(position: number): void;
-  releaseScb(): void;
-  alignScbToRight(): void;
-  removeClass(element: HTMLElement, className: string): void;
-  addClass(element: HTMLElement, className: string): void;
 }
 
 declare const Scroller: ScrollerConstructor;
