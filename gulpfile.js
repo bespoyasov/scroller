723d9c7703c8a78c8dfa19c6e15803932ff91105
Shakmatov Kirill <k.shakhmatov@mindset.digital>
2021-08-21 15:21:41 +0300

    Updated gulp and vinyl dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,68 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
 
-		postcss = require('gulp-postcss'),
-		autoprefixer = require('autoprefixer'),
-		csso = require('gulp-csso'),
-		rename = require('gulp-rename'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    csso = require('gulp-csso'),
+    rename = require('gulp-rename'),
 
-		browserify = require('browserify'),
-		babelify = require('babelify');
-		minify = require('gulp-minify'),
-		jshint = require('gulp-jshint'),
-		source = require('vinyl-source-stream'),
+    browserify = require('browserify'),
+    babelify = require('babelify');
+    minify = require('gulp-minify'),
+    jshint = require('gulp-jshint'),
+    source = require('vinyl-source-stream'),
 
-		concat = require('gulp-concat'),
-		notify = require('gulp-notify'),
-		fs = require('fs');
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    fs = require('fs');
 
-
-var browsers = ['last 2 versions', 'ios 7'];
-
-
-gulp.task('styles', function() {
-	var processors = [autoprefixer({browsers: browsers})];
-
-	return gulp.src('./dev/style.css')
-		.pipe(postcss(processors))
-    .pipe(gulp.dest('./build/'))
-    .pipe(notify('Styles task completed'));
-});
+const { watch, series } = gulp;
+const browsers = ['last 2 versions', 'ios 7'];
 
 
+const styles = () => {
+    const processors = [autoprefixer({browsers: browsers})];
 
-gulp.task('scripts', function() {
+    return gulp.src('./dev/style.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./build/'))
+        .pipe(notify('Styles task completed'));
+}
 
-	browserify('dev/scroller.js', {
-		entries: ['dev/scroller.js'], 
-		debug: true
-	})
-		.transform(babelify, {
-			presets: ['es2015'],
-			plugins: ['transform-class-properties']
-		})
-		.bundle()
-    .pipe(source('scroller.js'))
-    .pipe(gulp.dest('build/'))
-		.pipe(notify('Scripts task completed'));
-});
+const scripts = () => {
+    return browserify('dev/scroller.js', {
+        entries: ['dev/scroller.js'],
+        debug: true
+    })
+        .transform(babelify, {
+            presets: ['es2015'],
+            plugins: ['transform-class-properties']
+        })
+        .bundle()
+        .pipe(source('scroller.js'))
+        .pipe(gulp.dest('build/'))
+        .pipe(notify('Scripts task completed'));
+}
 
+const styles_minify = () => {
+    const processors = [autoprefixer({ browsers: browsers })];
 
+    return gulp.src('./dev/style.css')
+        .pipe(postcss(processors))
+        .pipe(csso())
+        .pipe(rename('styles-min.css'))
+        .pipe(gulp.dest('./build/'))
+        .pipe(notify('Styles-minify task completed'));
+}
 
-gulp.task('styles-minify', function() {
+const scripts_minify = () => {
+    watch('./dev/**/*.css', styles);
+    watch('./dev/**/*.js', scripts);
 
-	var processors = [autoprefixer({ browsers: browsers })];
-
-	return gulp.src('./dev/style.css')
-		.pipe(postcss(processors))
-		.pipe(csso())
-		.pipe(rename('styles-min.css'))
-    .pipe(gulp.dest('./build/'))
-    .pipe(notify('Styles-minify task completed'));
-});
-
-
-
-gulp.task('scripts-minify', function() {
-	return gulp.src('build/scroller.js')
-		.pipe(minify())
-    .pipe(gulp.dest('build/'))
-		.pipe(notify('Scripts-minify task completed'));
-});
-
-
-
-gulp.task('default', ['styles', 'scripts'], function(){
-	gulp.watch('./dev/**/*.css', ['styles']);
-	gulp.watch('./dev/**/*.js', ['scripts']);
-});
+    return gulp.src('build/scroller.js')
+        .pipe(minify())
+        .pipe(gulp.dest('build/'))
+        .pipe(notify('Scripts-minify task completed'));
+}
 
 
-gulp.task('build', ['styles-minify', 'scripts-minify']);
+exports.build = series(styles, scripts, styles_minify, scripts_minify);
