050a18fb2c5632404148770dbbd43e91a6779579
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-20 16:51:29 +0200

    Add a helper for setting `width`

--- src/dom.js
+++ src/dom.js
@@ -1,2 +1,4 @@
+export const setWidth = (el, value) => (el.style.width = `${value}px`);
+
 export const classIf = (element, condition, ...classNames) =>
   condition ? element.classList.add(...classNames) : element.classList.remove(...classNames);
