41471b449954755d064e21c156d158c578a14cc4
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-20 16:51:50 +0200

    Add a helpers for setting element translation

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,5 @@
 export const setWidth = (el, value) => (el.style.width = `${value}px`);
+export const setPosition = (el, value) => (el.style.transform = `translateX(${value}px)`);
 
 export const classIf = (element, condition, ...classNames) =>
   condition ? element.classList.add(...classNames) : element.classList.remove(...classNames);
