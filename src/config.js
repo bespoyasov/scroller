82e0248b6671aba4e2ae8e0622d65a0bbe3763a0
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-20 16:22:29 +0200

    Create a config factory

---
+++ src/config.js
@@ -0,0 +1,11 @@
+import { visibility } from "./const.js";
+
+export function createRuntimeConfig({ element, config }) {
+  const { dataset } = element;
+
+  return {
+    ...config,
+    scrollbar: config.scrollbar || dataset.scrollbar || visibility.visible,
+    navigation: config.navigation || dataset.navigation || visibility.visible,
+  };
+}
