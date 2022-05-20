916ebc0152eda1daadd32d1bf977390de1823c7a
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-20 16:22:38 +0200

    Specify runtime configs for the component

--- src/component.js
+++ src/component.js
@@ -1,13 +1,17 @@
 import { classNames } from "./classes.js";
+
+import { createRuntimeConfig } from "./config.js";
 import { createInitialState } from "./state.js";
 
 export class Scroller {
   static className = classNames.namespace;
 
-  constructor({ element }) {
+  constructor({ element, ...config }) {
     if (!element) throw new Error("The scroller element must be specified.");
 
     this.state = createInitialState();
+    this.config = createRuntimeConfig({ config, element });
+
     this.init();
   }
 
