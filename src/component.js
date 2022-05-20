b777de21735523965ece7da4bf09408265bc125c
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-20 15:50:02 +0200

    Specify a `className` static public field

--- src/component.js
+++ src/component.js
@@ -1,4 +1,8 @@
+import { classNames } from "./classes.js";
+
 export class Scroller {
+  static className = classNames.namespace;
+
   constructor({ element }) {
     if (!element) throw new Error("The scroller element must be specified.");
 
