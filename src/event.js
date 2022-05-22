6be35772095b7007e9cca79882df8c1ba92b992c
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-22 20:38:15 +0200

    Add an event coordinate selector

--- src/event.js
+++ src/event.js
@@ -1,3 +1,9 @@
 export function hasHorizontalDirection(event) {
   return Math.abs(event.deltaY) <= Math.abs(event.deltaX);
 }
+
+export function coordinatesOf(event, kind = "page") {
+  const data = event.changedTouches || event.touches || event;
+  const { [`${kind}X`]: x, [`${kind}Y`]: y } = data;
+  return { x, y };
+}
