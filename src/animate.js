f5235a84d3274bf96160844718501526a0b30591
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-22 20:36:33 +0200

    Rename `time` to `duration`

--- src/animate.js
+++ src/animate.js
@@ -1,8 +1,8 @@
 const easeOutExpo = (position) => (position === 1 ? 1 : -Math.pow(2, -10 * position) + 1);
 const nope = () => false;
 
-export function animateValue({ from, to, callback, stop = nope, time = 250 }) {
-  if (to === from || !time) return callback(to);
+export function animateValue({ from, to, callback, stop = nope, time: duration = 250 }) {
+  if (to === from || !duration) return callback(to);
 
   const distance = Math.abs(to - from);
   const forward = to > from;
@@ -16,12 +16,12 @@ export function animateValue({ from, to, callback, stop = nope, time = 250 }) {
 
     startedAt = startedAt || timestamp - 1;
     const elapsed = timestamp - startedAt;
-    const gain = Math.round(direction * distance * easeOutExpo(elapsed / time));
+    const gain = Math.round(direction * distance * easeOutExpo(elapsed / duration));
     const position = constraint(from + gain, to);
 
     callback(position);
 
-    if (elapsed >= time) return;
+    if (elapsed >= duration) return;
     if (forward && position >= to) return;
     if (!forward && position <= to) return;
     window.requestAnimationFrame(tick);
