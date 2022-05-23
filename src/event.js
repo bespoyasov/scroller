8ad8a2140029395d923e1f388ad47633117dbe2b
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-23 20:44:19 +0200

    Add a time coordinate

--- src/event.js
+++ src/event.js
@@ -4,6 +4,6 @@ export function hasHorizontalDirection(event) {
 
 export function coordinatesOf(event, kind = "page") {
   const data = event.changedTouches || event.touches || event;
-  const { [`${kind}X`]: x, [`${kind}Y`]: y } = data;
-  return { x, y };
+  const { [`${kind}X`]: x, [`${kind}Y`]: y, timeStamp: t } = data;
+  return { x, y, t };
 }
