ddf6a6a6e56da5bc5ed4147e0e81db32d0ad30ae
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-23 18:59:51 +0200

    Rename `duration` parameter in the public API

--- src/animate.js
+++ src/animate.js
@@ -1,7 +1,7 @@
 const easeOutExpo = (position) => (position === 1 ? 1 : -Math.pow(2, -10 * position) + 1);
 const nope = () => false;
 
-export function animateValue({ from, to, callback, stop = nope, time: duration = 250 }) {
+export function animateValue({ from, to, callback, stop = nope, duration = 250 }) {
   if (to === from || !duration) return callback(to);
 
   const distance = Math.abs(to - from);
