3eadc4d6fd8a7c511a464ddc97f7c0521662c474
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-25 18:32:51 +0200

    Introduce shrink factor and origin for the scroll handle elasticity at the scroller edges

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,10 @@
 export const setWidth = (el, value) => (el.style.width = `${value}px`);
 export const setPosition = (el, value) => el.style.setProperty("--position", `${value}px`);
 
+export const setShrink = (el, { factor, origin }) => {
+  el.style.setProperty("--shrink-factor", factor);
+  el.style.setProperty("--shrink-origin", origin);
+};
+
 export const classIf = (element, condition, ...classNames) =>
   condition ? element.classList.add(...classNames) : element.classList.remove(...classNames);
