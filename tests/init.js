15a0da24872661ca8d8fe22409d4f5287f23982d
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-27 14:11:41 +0200

    Add test cases for alignment specified via JS config

--- tests/init.js
+++ tests/init.js
@@ -15,6 +15,21 @@ document.addEventListener("DOMContentLoaded", () => {
     navigation: "hidden",
   });
 
+  new Scroller({
+    element: document.querySelector(".js-scroller-align-start"),
+    align: "start",
+  });
+
+  new Scroller({
+    element: document.querySelector(".js-scroller-align-center"),
+    align: "center",
+  });
+
+  new Scroller({
+    element: document.querySelector(".js-scroller-align-end"),
+    align: "end",
+  });
+
   console.log({ scroller1 });
   console.log({ scroller2 });
   console.log({ scroller3 });
