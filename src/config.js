a3817ad20c549832291aa19394930456e888804b
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-27 11:51:20 +0200

    Normalize the aligment value in the config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+import { contentAlignment } from "./alignment.js";
 import { visibility } from "./visibility.js";
 
 export function createRuntimeConfig({ element, config }) {
@@ -5,7 +6,10 @@ export function createRuntimeConfig({ element, config }) {
 
   return {
     ...config,
+
     scrollbar: config.scrollbar || dataset.scrollbar || visibility.visible,
     navigation: config.navigation || dataset.navigation || visibility.visible,
+
+    align: config.align || dataset.align || contentAlignment.center,
   };
 }
