34efbed0cc87757380587d6d1cacdfd283236a03
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-27 14:05:16 +0200

    Add test cases for element visibility settings set via JS config

--- tests/init.js
+++ tests/init.js
@@ -5,5 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
     element: document.querySelector(".js-scroller-1"),
   });
 
+  const scroller2 = new Scroller({
+    element: document.querySelector(".js-scroller-2"),
+    scrollbar: "hidden",
+  });
+
+  const scroller3 = new Scroller({
+    element: document.querySelector(".js-scroller-3"),
+    navigation: "hidden",
+  });
+
   console.log({ scroller1 });
+  console.log({ scroller2 });
+  console.log({ scroller3 });
 });
