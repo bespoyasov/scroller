7b55d20621df4b865e579e01e73b5a398b508f08
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-29 19:07:01 +0200

    Skip initialized elements in auto initialization

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { Scroller } from "./component.js";
 
 document.addEventListener("DOMContentLoaded", function initializeAll() {
-  document
-    .querySelectorAll(`.${Scroller.className}`)
-    .forEach((element) => new Scroller({ element }));
+  document.querySelectorAll(`.${Scroller.className}`).forEach((element) => {
+    if (element.dataset.initialized) return;
+    new Scroller({ element });
+  });
 });
 
 export { Scroller };
